85536d65ec9fde00bd716c089f629c665266e3d7
Riley <riley.bloomfield@gmail.com>
2015-09-19 16:16:47 -0400

    Testing saving client code

--- Ember/controllers/index.js
+++ Ember/controllers/index.js
@@ -2,6 +2,8 @@ PartyShark.IndexController = Ember.ObjectController.extend({
 	init: function() {
 		$.get( "/clientCode", function( data ) {
   			//Need to save the client code
+  			data.save();
+  			alert(this.store.find('clientCode'));
 		});
 	},
     actions: {
