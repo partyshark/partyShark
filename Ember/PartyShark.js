d7e4c4128f3934fa383ab9eac888e368c1f5a334
Riley <riley.bloomfield@gmail.com>
2015-09-19 17:13:44 -0400

    .

--- Ember/PartyShark.js
+++ Ember/PartyShark.js
@@ -1,5 +1,3 @@
 //Create Application
 PartyShark = Ember.Application.create();
 PartyShark.CodePackAdapter = DS.FixtureAdapter;
-//Local storage adapters
-//PartyShark.CodePackSerializer = DS.LSSerializer.extend();
